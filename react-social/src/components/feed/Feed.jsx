f6f375dca11cca9b1529dfd8edf3d004476da157
JoshuaR26 <joshua2009474@gmail.com>
2023-10-04 18:42:09 +0530

    Feed

--- react-social/src/components/feed/Feed.jsx
+++ react-social/src/components/feed/Feed.jsx
@@ -1,9 +1,12 @@
 import "./feed.css"
+import Share from "../share/Share"
 
 export default function Feed() {
   return (
     <div className="feed">
-        Feed
+        <div className="feedWrapper">
+          <Share/>
+        </div>
     </div>
   )
 }
---
+++ react-social/src/components/share/Share.jsx
@@ -0,0 +1,16 @@
+import "./share.css";
+
+export default function Share() {
+  return (
+    <div className="share">
+        <div className="shareWrapper">
+            <div className="shareTop">
+                <img src="" alt="" className="shareProfile" />
+            </div>
+            <div className="shareBottom">
+
+            </div>
+        </div>
+    </div>
+  )
+}
