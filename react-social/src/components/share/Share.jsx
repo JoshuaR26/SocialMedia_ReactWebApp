77cbcac2e135733bf169e33b15222f486f518123
JoshuaR26 <joshua2009474@gmail.com>
2023-10-05 00:40:02 +0530

    Share

--- react-social/src/components/share/Share.jsx
+++ react-social/src/components/share/Share.jsx
@@ -1,14 +1,22 @@
 import "./share.css";
+import { PermMedia } from "@mui/icons-material";
 
 export default function Share() {
   return (
     <div className="share">
         <div className="shareWrapper">
             <div className="shareTop">
-                <img src="" alt="" className="shareProfile" />
+                <img src="../assets/person/1.jpg" alt="" className="shareProfileImg" />
+                <input placeholder="What's on your mind?" className="shareInput" />
             </div>
+            <hr className="shareHr" />
             <div className="shareBottom">
-
+              <div className="shareOptions">
+                <div className="shareOption">
+                  <PermMedia className="shareIcon"/>
+                  <span className="shareOptionText">Photo/Video</span>
+                </div>
+                </div>
             </div>
         </div>
     </div>
