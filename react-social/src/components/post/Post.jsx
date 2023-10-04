508f6d5ef76c2ae72c4f92d11c1a695c72451668
JoshuaR26 <joshua2009474@gmail.com>
2023-10-05 02:14:47 +0530

    Post updt

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -1,4 +1,5 @@
 import "./post.css";
+import {AddComment, Bookmark, Comment, FavoriteBorder, MoreVert, Share} from "@mui/icons-material";
 
 export default function Post() {
   return (
@@ -6,19 +7,30 @@ export default function Post() {
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src="asset/person/2.jpg" alt="" className="postProfileImg" />
+                    <img src="assets/person/2.jpg" alt="" className="postProfileImg" />
+                    <span className="postUsername">David Hasselhoff</span>
+                    <span className="postDate">5min ago</span>
                 </div>
                 <div className="postTopRight">
-
+                    <MoreVert/>
                 </div>
             </div>
 
             <div className="postCenter">
-
+                <span className="postCaption">Wasssssuppp y'all! </span>
+                <img src="assets/post/1.jpg" alt="" className="postImg" />
             </div>
 
             <div className="postBottom">
-
+                <div className="postBottomLeft">
+                    <FavoriteBorder className="postBottomIcon"/>
+                    <AddComment className="postBottomIcon"/>
+                    <Comment className="postBottomIcon"/>
+                </div>
+                <div className="postBottomRight">
+                    <Bookmark className="postBottomIcon"/>
+                    <Share  className="postBottomIcon"/>
+                </div>
             </div>
         </div>
     </div>
