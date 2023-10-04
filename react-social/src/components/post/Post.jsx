b52355935df1d551338b3a23e3ecd49811502fb7
JoshuaR26 <joshua2009474@gmail.com>
2023-10-05 01:27:04 +0530

    post

---
+++ react-social/src/components/post/Post.jsx
@@ -0,0 +1,26 @@
+import "./post.css";
+
+export default function Post() {
+  return (
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img src="asset/person/2.jpg" alt="" className="postProfileImg" />
+                </div>
+                <div className="postTopRight">
+
+                </div>
+            </div>
+
+            <div className="postCenter">
+
+            </div>
+
+            <div className="postBottom">
+
+            </div>
+        </div>
+    </div>
+  )
+}
--- react-social/src/components/share/Share.jsx
+++ react-social/src/components/share/Share.jsx
@@ -1,5 +1,5 @@
 import "./share.css";
-import { PermMedia } from "@mui/icons-material";
+import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material";
 
 export default function Share() {
   return (
@@ -13,11 +13,27 @@ export default function Share() {
             <div className="shareBottom">
               <div className="shareOptions">
                 <div className="shareOption">
-                  <PermMedia className="shareIcon"/>
+                  <PermMedia htmlColor="gray" className="shareIcon"/>
                   <span className="shareOptionText">Photo/Video</span>
                 </div>
+
+                <div className="shareOption">
+                  <Label htmlColor="gray" className="shareIcon"/>
+                  <span className="shareOptionText">Tag</span>
+                </div>
+
+                <div className="shareOption">
+                  <Room htmlColor="gray" className="shareIcon"/>
+                  <span className="shareOptionText">Location</span>
+                </div>
+
+                <div className="shareOption">
+                  <EmojiEmotions htmlColor="gray" className="shareIcon"/>
+                  <span className="shareOptionText">Feeelings</span>
                 </div>
             </div>
+            <button className="shareButton">Share</button>
+          </div>
         </div>
     </div>
   )
