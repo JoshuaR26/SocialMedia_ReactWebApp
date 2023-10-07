f0754892eef2cf29a5dbdd24447ad4f4bae48e9b
JoshuaR26 <joshua2009474@gmail.com>
2023-10-07 19:32:35 +0530

    RightBar updt

--- react-social/src/components/righbar/RightBar.jsx
+++ react-social/src/components/righbar/RightBar.jsx
@@ -3,7 +3,9 @@ import "./rightbar.css"
 export default function RightBar() {
   return (
     <div className="rightBar">
-        RightBar
+        <div className="rightbarWrapper">
+          
+        </div>
     </div>
   )
 }
